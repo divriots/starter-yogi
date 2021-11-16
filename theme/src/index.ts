d7b2b60998ad6b0d50a57b215aabed6c8fa9d2bc
Mihai <7051248+undefined@users.noreply.github.com>
2021-11-16 13:43:48 +0000

    z-index, switch and border fixes

--- theme/src/index.ts
+++ theme/src/index.ts
@@ -1,12 +1,12 @@
 import merge from 'deepmerge';
-import space from '../../space/src';
-import sizes from '../../sizes/src';
-import typography from '../../typography/src';
-import colors from '../../colors/src';
-import shadows from '../../shadows/src';
-import radii from '../../radii/src';
-//import border from '../../border/src';
-import zIndices from '../../z-index/src';
+import space from '~/space/src';
+import sizes from '~/sizes/src';
+import typography from '~/typography/src';
+import colors from '~/colors/src';
+import shadows from '~/shadows/src';
+import radii from '~/radii/src';
+import borders from '~/border/src';
+import zIndices from '~/z-index/src';
 import mdx from './mdx';
 
 export const theme = merge.all(
@@ -17,7 +17,8 @@ export const theme = merge.all(
     { colors },
     { shadows },
     { radii },
-    /*border,*/ { zIndices },
+    { borders },
+    { zIndices },
     { mdx },
   ],
   { arrayMerge: (t, s) => [...s, ...t] }
