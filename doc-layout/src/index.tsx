230824f4677df2f9cc562c86299dabd6c4562611
Mihai <7051248+undefined@users.noreply.github.com>
2021-11-17 09:03:10 +0000

    Dark Mode feature for docs

--- doc-layout/src/index.tsx
+++ doc-layout/src/index.tsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import * as React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { CoreLayout } from '@divriots/dockit-react/mdx-layout-core/dist/CoreLayout';
+import {
+  ChakraProvider,
+  extendTheme,
+  useColorMode,
+  ColorModeScript,
+  ThemeConfig,
+} from '@chakra-ui/react';
+import {
+  CoreLayout,
+  StylesheetSwitch,
+} from '@divriots/dockit-react/mdx-layout-core';
 import { theme } from '~/theme';
 import { components } from './components';
 import './style.css';
+import { Logo } from './Logo';
 
 // extend default Chakra theme
-const fullTheme = extendTheme(theme);
+const config: ThemeConfig = {
+  initialColorMode: 'system',
+  useSystemColorMode: false,
+};
 
-const ThemeProviderLayout = (props) => (
-  <MDXProvider components={components}>
-    <ChakraProvider theme={fullTheme}>
-      <CoreLayout
-        logo={
-          <img
-            src="https://storage.googleapis.com/prd-assets/yogi-color.svg"
-            style={{ width: '8rem' }}
+const fullTheme = extendTheme({ ...theme, config });
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <StylesheetSwitch
+      colorScheme={colorMode}
+      onSwitch={() => toggleColorMode()}
+    />
+  );
+};
+
+const ThemeProviderLayout = (props) => {
+  return (
+    <>
+      <ColorModeScript initialColorMode="system" />
+      <MDXProvider components={components}>
+        <ChakraProvider theme={fullTheme}>
+          <CoreLayout
+            stylesheetSwitch={<ColorModeSwitch />}
+            logo={
+              <div style={{ width: '8rem' }}>
+                <Logo />
+              </div>
+            }
+            {...props}
           />
-        }
-        {...props}
-      />
-    </ChakraProvider>
-  </MDXProvider>
-);
+        </ChakraProvider>
+      </MDXProvider>
+    </>
+  );
+};
 
 export default ThemeProviderLayout;
